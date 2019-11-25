9ee88f835b92266f5ac26dac2e8783c89e118cfd
Gustavo Felipe da Silva <gustavo.silva@dtidigital.com.br>
2019-11-25 13:44:12 -0300

    att nova

---
+++ TesteAngular.Web/ClientApp/src/app/produto/produto.component.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ProdutoComponent = /** @class */ (function () {
+    function ProdutoComponent() {
+        this.name = "Produto Teste";
+    }
+    return ProdutoComponent;
+}());
+exports.ProdutoComponent = ProdutoComponent;
+//# sourceMappingURL=produto.component.js.map
---
+++ TesteAngular.Web/ClientApp/src/app/produto/produto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'produton',
+  templateUrl: './produto.component.html',
+  styleUrls: ['./produto.component.css']
+})
+export class ProdutoComponent {
+  public names = ["Computador", " Celular", " Caneta"];
+}
